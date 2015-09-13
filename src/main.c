2f6752c3e233b81c8ee34bcc4b7478885c74c326
clach04 <Chris.Clark@actian.com>
2015-09-12 18:12:19 -0700

    Start to refactor

--- src/main.c
+++ src/main.c
@@ -7,48 +7,40 @@
 Window *my_window;
 Layer *text_layer;
 char lorem[64];
- 
-void draw_custom(Layer *this_layer, GContext *ctx){
+
+void draw_text(GContext *ctx, char * the_text, GFont the_font, int16_t x, int16_t y, int16_t w, int16_t h)
+{
     graphics_context_set_text_color(ctx, GColorBlack);
-    
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(1,1, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(3,1, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(1,3, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(3,3, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), GRect(1,31, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL); 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), GRect(3,31, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), GRect(1,33, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), GRect(3,33, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-   
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28), GRect(1,61, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28), GRect(3,61, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28), GRect(1,63, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28), GRect(3,63, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), GRect(1,91, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), GRect(3,91, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), GRect(1,93, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), GRect(3,93, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK), GRect(1,121, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK), GRect(3,121, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK), GRect(1,123, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK), GRect(3,123, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-    
+    //graphics_draw_text(ctx, the_text, the_font, GRect(1,1, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, the_text, the_font, GRect(x,y, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+
+    //graphics_draw_text(ctx, the_text, the_font, GRect(3,1, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, the_text, the_font, GRect(x+2,y, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+
+    //graphics_draw_text(ctx, the_text, the_font, GRect(1,3, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, the_text, the_font, GRect(x,y+2, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+
+    //graphics_draw_text(ctx, the_text, the_font, GRect(3,3, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, the_text, the_font, GRect(x+2,y+2, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+
     graphics_context_set_text_color(ctx, GColorWhite);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(2,2, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), GRect(2,32, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28), GRect(2,62, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), GRect(2,92, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
- 
-    graphics_draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK), GRect(2,122, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+    //graphics_draw_text(ctx, the_text, the_font, GRect(2,2, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, the_text, the_font, GRect(x+1,y+1, w,h), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
 }
- 
+
+void draw_custom(Layer *this_layer, GContext *ctx){
+    draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14), 1,1, 144,32);
+
+    draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), 1,31, 144,32);
+
+    draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28), 1,61, 144,32);
+
+    draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD), 1,91, 144,32);
+
+    draw_text(ctx, lorem, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK), 1,121, 144,32);
+
+}
+
 void handle_init(void) {
     my_window = window_create();
     strcpy(lorem, "Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam.");
